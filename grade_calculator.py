d167f2cba8ee717ab81c648f7a6711b6be81912f
Bryan Garrett <67705098+BuckG71@users.noreply.github.com>
2024-05-14 17:15:35 -0500

    updated grade_calculator.py fix error in main function

--- grade_calculator.py
+++ grade_calculator.py
@@ -5,22 +5,22 @@ def calculate_average_grade():
 
     # Prompt the user for their scores in Math, Science, and English
     # Store the scores in the respective variables: math_score, science_score, english_score
-    # I used the input function to prompt the user for the needed inputs
-    math_score = input("What was your Math score? ")
-    science_score = input("What was your Science score? ")
-    english_score = input("What was your English score? ")
+    # I used the input function to prompt the user for the needed inputs and simultaneously converted the input from a string to a float using the float function
+    math_score = float(input("What was your Math score? "))
+    science_score = float(input("What was your Science score? "))
+    english_score = float(input("What was your English score? "))
 
     # Calculate the average grade
-    '''Since user inputs are strings, I type converted them to integers before calculating the average grade'''
-    average_grade = (int(math_score) + int(science_score) + int(english_score)) / 3
+    # Used simple arithmetic to calculate the average grade
+    average_grade = (math_score + science_score + english_score) / 3
 
     # Return the student's name and their average grade
-    ''' I've added a print statement here to print the student's name and their average grade, and I type converted the average grade to an integer since school grades are usually whole numbers'''
-    return print(f"{student_name}'s average grade is {int(average_grade)}.")
+    # I've added a print f statement here to print the student's name and their average grade.
+    return print(f"{student_name}'s average grade is {average_grade}.")
 
 if __name__ == '__main__':
     # Call the calculate_average_grade function
-    student_name, average_grade = calculate_average_grade()
+    calculate_average_grade()
 
     # Print the student's name and their average grade
-    print(f"")
+    # print(f"{student_name}'s average grade is {average_grade}")
